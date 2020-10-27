8ac3c7c260328a7399ba2558a2d06f030c607e26
vitor0x5 <vitorhugo.glh@gmail.com>
2020-10-27 00:11:33 -0300

    Create the ToastContainer component and styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,15 @@ import SignUp from './pages/SignUp';
 
 import { AuthProvider } from './hooks/AuthContext';
 
+import ToastContainer from './components/ToastContainer';
+
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
